fc20179b40314fe3c0ce3813e2fa029657b6c2a9
annaloustarr <61509489+annaloustarr@users.noreply.github.com>
2020-03-15 22:06:26 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+document.getElementById("loader").classList.add("hidden");
+document.getElementById("searchResults").classList.add("hidden");
+
+let listDiv = document.getElementById("dataList");
+
+function getCompanyData(x) {
+  document.getElementById("loader").classList.remove("hidden");
+  document.getElementById("searchResults").classList.remove("hidden");
+  fetch(
+    "https://financialmodelingprep.com/api/v3/search?query=AA&limit=10&exchange=NASDAQ"
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      let companyList = data;
+      console.log(companyList);
+      for (let i = 0; i < companyList.length; i++) {
+        let li = document.createElement("li");
+        let companyName = document.createElement("span");
+        companyName.innerText = companyList[i].name;
+        let companySymbol = document.createElement("span");
+        companySymbol.innerText = " (" + companyList[i].symbol + ")";
+        li.append(companyName, companySymbol);
+        listDiv.appendChild(li);
+      }
+      document.getElementById("loader").classList.add("hidden");
+    });
+}
+
+document
+  .getElementById("searchButton")
+  .addEventListener("click", getCompanyData);
