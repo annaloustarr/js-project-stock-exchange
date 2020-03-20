95ac02495cf229aa34986786a787827202e416aa
Anna Starr <anna.starr@gmail.com>
2020-03-20 17:57:05 +0200

    completed milestone 5

--- js/marquee.js
+++ js/marquee.js
@@ -1,44 +1,51 @@
-// function to make element is html to add list to
-function createMarqueeElement() {
-  let marqueediv = document.getElementById("marquee");
-  let div = document.createElement("div");
-  div.classList.add("marquee-content");
-  let ul = document.createElement("ul");
-  ul.id = "marqueeList";
-  ul.classList.add("list-inline");
-  div.appendChild(ul);
-  marqueediv.appendChild(div);
+let marqueeDiv = document.getElementById("marquee");
+
+class Marquee {
+  constructor(element) {
+    this.element = element;
+  }
+
+  // class method to make element in html to add list to
+  createMarqueeElement() {
+    let innerDiv = document.createElement("div");
+    innerDiv.classList.add("marquee-content");
+    let ul = document.createElement("ul");
+    ul.id = "marqueeList";
+    ul.classList.add("list-inline");
+    innerDiv.appendChild(ul);
+    this.element.appendChild(innerDiv);
+  }
+
+  // class method to make list for marquee
+  createMarqueeList() {
+    fetch(`https://financialmodelingprep.com/api/v3/stock/real-time-price`)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        let stockList = data.stockList;
+        let marqueeDiv = document.getElementById("marqueeList");
+        for (let i = 0; i < 250; i++) {
+          let companySymbol = stockList[i].symbol;
+          let companyPrice = stockList[i].price;
+
+          let li = document.createElement("li");
+          let symbolText = document.createElement("span");
+          symbolText.innerHTML = companySymbol;
+          let priceText = document.createElement("span");
+          priceText.innerHTML = " $" + companyPrice;
+          priceText.classList.add("lightgreen");
+          li.append(symbolText, priceText);
+
+          marqueeDiv.appendChild(li);
+        }
+      });
+  }
 }
 
-createMarqueeElement();
-
-// function to make list for marquee
-function createMarqueeList() {
-  fetch(`https://financialmodelingprep.com/api/v3/stock/real-time-price`)
-    .then(response => {
-      return response.json();
-    })
-
-    .then(data => {
-      let stockList = data.stockList;
-      let marqueeDiv = document.getElementById("marqueeList");
-      for (let i = 0; i < 250; i++) {
-        let companySymbol = stockList[i].symbol;
-        let companyPrice = stockList[i].price;
-
-        let li = document.createElement("li");
-        let symbolText = document.createElement("span");
-        symbolText.innerHTML = companySymbol;
-        let priceText = document.createElement("span");
-        priceText.innerHTML = " $" + companyPrice;
-        priceText.classList.add("lightgreen");
-        li.append(symbolText, priceText);
-
-        marqueeDiv.appendChild(li);
-      }
-    });
-}
-createMarqueeList();
+let myMarquee = new Marquee(marqueeDiv);
+myMarquee.createMarqueeElement();
+myMarquee.createMarqueeList();
 
 //   keep map here incase I want all 10000+
 //   stockList.map(item => {
